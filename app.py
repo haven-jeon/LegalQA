90454ee5311d8188b1f68e8799ecf80655b8bb6c
haven-jeon <gogamza@gmail.com>
2023-03-25 16:56:51 +0900

    add chat demo page

--- app.py
+++ app.py
@@ -20,6 +20,7 @@ def config():
 def print_topk(resp, sentence):
     for doc in resp.data.docs:
         print(f"\n\n\nTa-Dah🔮, here's what we found for: {sentence}")
+        print(">> " + doc.tags['ai_response'])
         for idx, match in enumerate(doc.matches):
             score = match.scores['cosine'].value
             bert_score = match.scores['bert_rerank'].value
@@ -38,13 +39,6 @@ def _pre_processing(texts):
                            'question': d['question'],
                            'answer': d['answer']
                        })
-
-def _pre_processing_note(data_path: str):
-    jd = json.load(open(data_path, 'r'))
-    for l in jd:
-        #l.pop('id', None)
-        yield Document(id = l['id'], text= l['text'], tags=l['tags'])
-
 
 def index(index_flow):
     f = Flow.load_config(index_flow)
@@ -101,6 +95,7 @@ def query_restful(query_flow):
                              'protocol': 'http',
                              'port': int(os.environ["JINA_PORT"]),
                          })
+    f.plot(output='query_restful.svg')
     with f:
         f.block()
 
@@ -115,7 +110,7 @@ def query_restful(query_flow):
 @click.option("--top_k", "-k", default=3)
 @click.option('--flow',
               type=click.Path(exists=True),
-              default='flows/query_numpy.yml')
+              default='flows/query_annlite.yml')
 def main(task, top_k, flow):
     config()
     workspace = os.environ["JINA_WORKSPACE"]
